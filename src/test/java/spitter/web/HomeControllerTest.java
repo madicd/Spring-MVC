ff618952332843f726de8016392b6165a61a3ab6
Djordje Madic <madj@zuehlke.com>
2017-09-25 00:26:49 +0200

    Added unit test for HomeController

---
+++ src/test/java/spitter/web/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package spitter.web;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomeControllerTest {
+    @Test
+    public void testHomePage() throws Exception {
+        HomeController controller = new HomeController();
+        assertEquals("home", controller.home());
+    }
+
+}
