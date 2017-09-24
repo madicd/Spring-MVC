3d9de76303e128ce74f4ac6c1a67fad07159f441
Djordje Madic <madj@zuehlke.com>
2017-09-25 00:31:05 +0200

    Changed unit test to use MockMvc testing

--- src/test/java/spitter/web/HomeControllerTest.java
+++ src/test/java/spitter/web/HomeControllerTest.java
@@ -1,14 +1,24 @@
 package spitter.web;
 
 import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 public class HomeControllerTest {
     @Test
     public void testHomePage() throws Exception {
         HomeController controller = new HomeController();
-        assertEquals("home", controller.home());
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(view().name("home"));
     }
 
 }
