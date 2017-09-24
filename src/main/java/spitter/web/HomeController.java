96d12362e3729504ac5f5501c8e1ca13b4d35c58
Djordje Madic <madj@zuehlke.com>
2017-09-25 01:04:16 +0200

    Added class level request handling

--- src/main/java/spitter/web/HomeController.java
+++ src/main/java/spitter/web/HomeController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
+@RequestMapping({"/", "/homepage"})
 public class HomeController {
 
-    @RequestMapping(value = "/", method = GET)
+    @RequestMapping(method = GET)
     public String home() {
         return "home";
     }
