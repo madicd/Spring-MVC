7dcefffb468e201446da34fc2e9893d7aded0e36
Djordje Madic <madicdjordje@gmail.com>
2017-10-05 22:58:52 +0200

    5.3.1 Taking query parameters

--- src/main/java/spitter/web/SpittleController.java
+++ src/main/java/spitter/web/SpittleController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import spitter.Spittle;
 import spitter.data.SpittleRepository;
+
+import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -20,8 +24,11 @@ public class SpittleController {
     }
 
     @RequestMapping(method = GET)
-    public String spittles(Model model) {
-        model.addAttribute("spittleList", spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+    public String spittles(@RequestParam(value = "maxId", defaultValue = "" + Long.MAX_VALUE) long maxId,
+                           @RequestParam(value = "count", defaultValue = "20") int count,
+                           Model model) {
+        final List<Spittle> spittles = spittleRepository.findSpittles(maxId, count);
+        model.addAttribute("spittleList", spittles);
         return "spittles";
     }
 }
--- src/test/java/spitter/web/SpittleControllerTest.java
+++ src/test/java/spitter/web/SpittleControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 public class SpittleControllerTest {
     @Test
     public void shouldShowRecentSpittles() throws Exception {
-        List<Spittle> expectedSpittles = createSpittleList();
+        List<Spittle> expectedSpittles = createSpittleList(20);
         SpittleRepository mockRepository = mock(SpittleRepository.class);
         when(mockRepository.findSpittles(Long.MAX_VALUE, 20))
                 .thenReturn(expectedSpittles);
@@ -39,8 +39,27 @@ public class SpittleControllerTest {
 
     }
 
-    private List<Spittle> createSpittleList() {
-        return IntStream.range(0, 20)
+    @Test
+    public void shouldShowPagedSpittles() throws Exception {
+        List<Spittle> expectedSpittles = createSpittleList(50);
+        SpittleRepository mockRepository = mock(SpittleRepository.class);
+        when(mockRepository.findSpittles(238900, 50))
+                .thenReturn(expectedSpittles);
+
+        SpittleController controller = new SpittleController(mockRepository);
+        MockMvc mockMvc = standaloneSetup(controller)
+                .setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp"))
+                .build();
+
+        mockMvc.perform(get("/spittles?maxId=238900&count=50"))
+                .andExpect(view().name("spittles"))
+                .andExpect(model().attributeExists("spittleList"))
+                .andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
+
+    }
+
+    private List<Spittle> createSpittleList(int listSize) {
+        return IntStream.range(0, listSize)
                 .mapToObj(index -> new Spittle("Spittle " + index, new Date()))
                 .collect(toList());
     }
