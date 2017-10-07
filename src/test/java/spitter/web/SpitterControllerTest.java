18e30df1c41921ad6416e87daf28393c0919b327
Djordje Madic <madicdjordje@gmail.com>
2017-10-08 00:23:46 +0200

    5.4.0 Provide form registration page

---
+++ src/test/java/spitter/web/SpitterControllerTest.java
@@ -0,0 +1,21 @@
+package spitter.web;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class SpitterControllerTest {
+    @Test
+    public void shouldShowRegistrationForm() throws Exception {
+        SpitterController controller = new SpitterController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/spitter/register"))
+                .andExpect(view().name("registerForm"));
+    }
+}
